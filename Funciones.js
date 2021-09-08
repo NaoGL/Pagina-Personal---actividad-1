8a38d9fd0a4c347b7cf67f35821bf3155423d39e
NaoGL <82893990+NaoGL@users.noreply.github.com>
2021-09-08 13:39:17 -0500

    Add files via upload

---
+++ Funciones.js
@@ -0,0 +1,23 @@
+function reseña(num)
+{
+	if (num == 1) 
+		{
+			var reseña = 'Siempre me han llamado la atención los libros de terror, en especial de Stephen King tomando en cuenta la gran reputación que tiene. Por ello decidí comprar este libro y debo admitir que no me arrepiento para nada. No es un libro para cualquiera, se debe tener cierto valor para leerlo y tomarle el gusto, pero a mi parecer Misery es un estupendo libro que se volvió de mis favoritos hasta ahora, me hizo sentir un sin fin de emociones pero en especial la incertidumbre de saber como continuaría la historia y pavor por las escenas que se relataban.\nLe daría una calificación de 9/10'
+			return reseña;
+		}
+	else if (num == 2)
+		{
+			var reseña='No hace mucho que termine de leer este libro, creo que maneja bien la psicología de cada uno de los personajes explicando el porqué actuan de cierta manera o el porqué de sus acciones y conforme el libro va avanzando se detonan sucesos importantes para la trama que conectan varios aspectos del libro. El terror, aunque no es muy explicito, si que está presente pero considero yo que es más simbolico que otra cosa.\nLe daría una calificación de 8/10'
+			return reseña;
+		}
+	else if (num == 3)
+		{
+			var reseña='La caja de botones de Gwendy fue un libro corto pero interesante, plantea algunos dilemas al personaje y responsabilidades que tiene que afrontar pero poco a poco las va dejando de lado. Debo admitir que el final no me fue muy disfrutable, esperaba algo más.\nLe daría una calificación de 7/10'
+			return reseña;
+		}
+	else if (num == 4)
+		{
+			var reseña='Actualmente estoy leyendo La princesa que creía en los Cuentos de Hadas, un libro con un aprendizaje importante: el amor propio. Aunque al principio me pareció algo aburrido y muy meloso conforme avanzó la historia se vuelve más motivadora. Está lleno de frases que puedo aplicar a mi propia vida, sin duda lo recomendaría, estoy muy ansiosa por ver el final.\nLe daría una calificación de 8/10'
+			return reseña;
+		}
+}
